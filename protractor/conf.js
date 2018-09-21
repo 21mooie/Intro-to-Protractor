b85101bcb0ce0d64211c45d3bb997e9ac5057c3d
x222146 <muata.nkosi@prudential.com>
2018-09-21 15:48:20 -0400

    Use suites for testing relating code in Protractor

--- protractor/conf.js
+++ protractor/conf.js
@@ -2,6 +2,12 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./ratings/create.event.spec.js'],
 
+    suites: {
+        smoke: './smoke/*.spec.js',
+        longRunning: './ratings/longRunning.spec.js',
+        full:'./**/*.spec.js'
+    },
+
     onPrepare: function(){
         browser.driver.manage().window().setPosition(0,0);
         browser.driver.manage().window().setSize(1280,720);
