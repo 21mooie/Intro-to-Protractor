8aee56e825006ad493483a3fbf26d1d7a0d79d9a
Muata Nkosi <mnkosi@fandm.edu>
2018-09-20 22:00:21 -0400

    Use Protractor link locator to automate finding clicking on an anchor tag

--- protractor/ratings/event.list.item.spec.js
+++ protractor/ratings/event.list.item.spec.js
@@ -1,6 +1,6 @@
 describe('Event List Details: ', () =>{
     describe('When clicking on an event', () =>{
-        it('Should navigate to the details page', () => {            
+        beforeEach(function(){
             browser.get('http://localhost:3000/#!/EventRatings');
 
             var firstElement = element.all(by.binding('name')).first()
@@ -14,11 +14,17 @@ describe('Event List Details: ', () =>{
             firstElement.click();
 
             browser.waitForAngular();
+        })
 
+        it('Should navigate to the details page', () => {            
             var header = element(by.binding('name'));
 
             expect(header.getText()).toMatch('Jazz On The Green');
 
+        });
+
+        it('Should update the url',function(){
+            expect(browser.getCurrentUrl()).toMatch('EventRatings/');
         })
     })
 })
